97fa35369f7f301871b9efeaf166cf2f8e14827c
Veronica De Ronzi <veronica.deronzi@gmail.com>
2022-12-29 10:28:30 +0100

    linking frontend

--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'build/index.html', content_type='text/javascript')
+    return render(request, 'build/index.html')
