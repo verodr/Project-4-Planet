4f6cf153ad8e93802b471890d864e89fbfc585ec
Sal@Dell <salvatore.stefanelli@gmail.com>
2023-01-06 16:34:00 +0000

    fixing settings.py

--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django.contrib.postgres',
+    'django_on_heroku',
     'corsheaders',
     'jwt_auth',
     'contents',
